567f2c468a26affe60a670ad83ae892ceda264d1
binaya161 <90383987+binaya161@users.noreply.github.com>
2021-09-20 13:25:46 +0100

    Add files via upload
    This is code is used to practice rebase purpose

--- Programme/HelloWorld.java
+++ Programme/HelloWorld.java
@@ -2,7 +2,13 @@ package Programme;
 
 public class HelloWorld {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println ("I am the programme made for git");
+=======
+
+        System.out.println ("I am the programme made for git");
+        System.out.println ("this is commit into master");
+>>>>>>> Add files via upload
     }
 
     void hello(){
